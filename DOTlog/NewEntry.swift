c49130a9fcd9a93fc22f3be9c11038726224fb55
William Showalter <williamshowalter@gmail.com>
2015-03-22 21:33:57 -0800

    Layout updates - text box resizes automatically.

--- DOTlog/NewEntry.swift
+++ DOTlog/NewEntry.swift
@@ -13,14 +13,20 @@ import CoreData
 class NewEntry: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
 	let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
-	var categories = ["Hazard","Airport Closure","Kittens on Runway"]
+	var categories : [Int : String] = [0:"Hazard", 1:"Airport Closure", 2:"Kittens on Runway"]
+	var airports : [Int : String] = [0:"FBX", 1:"KZB", 2:"UZB", 3:"LNK"]
+
 	@IBOutlet var pickerCategories: UIPickerView! = UIPickerView()
+	@IBOutlet var pickerAirports: UIPickerView! = UIPickerView()
 
 	@IBOutlet weak var textCategory: UITextField!
 	@IBOutlet weak var textEvent: UITextView!
 	@IBOutlet weak var textEventTime: UITextField!
 	@IBOutlet weak var textEventDate: UITextField!
+	@IBOutlet weak var textAirport: UITextField!
 
+	@IBOutlet weak var labelStatus: UILabel!
+	
 	@IBAction func editEventTime(sender: UITextField) {
 		var datePickerView  : UIDatePicker = UIDatePicker()
 		datePickerView.datePickerMode = UIDatePickerMode.Time
@@ -60,7 +66,9 @@ class NewEntry: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
 		textEventDate.text = dateFormatter.stringFromDate(todaysDate)
 
 		textCategory.text = categories[0]
-		pickerCategories.delegate = self;
+		textAirport.text = airports[0]
+		pickerCategories.delegate = self
+		pickerAirports.delegate = self
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -68,14 +76,58 @@ class NewEntry: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
 		// Dispose of any resources that can be recreated.
 	}
 
-	// Get rid of the keyboard
+	// Get rid of the keyboard when touching outside
 	override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
 		self.view.endEditing(true);
 	}
-	
+	// Get rid of keyboard when hitting return
 	func textFieldShouldReturn(textField: UITextField!) -> Bool {
 		textField.resignFirstResponder();
 		return true;
+	}
+
+	// returns the number of 'columns' to display.
+	func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+		return 1
+	}
+
+	// returns the # of rows in each component..
+	func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+		if (pickerView == pickerCategories){
+			return categories.count
+		}
+		else if (pickerView == pickerAirports){
+			return airports.count
+		}
+		return categories.count
+	}
+
+	func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! {
+		if (pickerView == pickerCategories){
+			return categories[row]
+		}
+		else if (pickerView == pickerAirports){
+			return airports[row];
+		}
+		return categories[row]
+	}
+
+	func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int)
+	{
+		if (pickerView == pickerCategories){
+			textCategory.text = categories[row]
+		}
+		else if (pickerView == pickerAirports){
+			textAirport.text = airports[row];
+		}
+	}
+
+	@IBAction func editCategories(sender: UITextField) {
+		sender.inputView = pickerCategories
+	}
+
+	@IBAction func editAirport(sender: UITextField) {
+		sender.inputView = pickerAirports;
 	}
 
 	@IBAction func saveLogEntry(sender: AnyObject) {
@@ -86,42 +138,28 @@ class NewEntry: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
 		let logEntry = LogEntry(entity: entityDescription!,
 			insertIntoManagedObjectContext: managedObjectContext)
 
+		let tempAirport = (airports as NSDictionary).allKeysForObject(textAirport.text)[0] as Int;
+		let tempCategory = 0;
+		let tempUser = 0;
+
+		logEntry.entryDescription = textEvent.text
+		logEntry.airportID = tempAirport
+		logEntry.categoryID = tempCategory
+		logEntry.userID = tempUser
+
 		var error: NSError?
 
 		managedObjectContext?.save(&error)
 
 		if let err = error {
-			// Error to label
+			labelStatus.text = err.localizedFailureReason;
 		} else {
-			// Clear fields
+			labelStatus.text = "Log Entry Saved"
+			self.textEvent.text = ""
+			self.textCategory.text = categories[0]
+			self.viewDidLoad()
 		}
 
-		self.textEvent.text = ""
-		self.textCategory.text = categories[0]
-		self.viewDidLoad()
-	}
-
-	// returns the number of 'columns' to display.
-	func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
-		return 1
-	}
-
-	// returns the # of rows in each component..
-	func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-		return categories.count
-	}
-
-	func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! {
-		return categories[row]
-	}
-
-	func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int)
-	{
-		textCategory.text = categories[row]
-	}
-
-	@IBAction func editCategories(sender: UITextField) {
-		sender.inputView = pickerCategories
 	}
 
 }
