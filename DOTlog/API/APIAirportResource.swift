77ee268d985daace2e6a3eec9a1d31b784f86ceb
William Showalter <williamshowalter@gmail.com>
2015-04-30 14:01:53 -0800

    Outline of structure for new Airport API entry. Need to make objects and relate them properly.

--- DOTlog/API/APIAirportResource.swift
+++ DOTlog/API/APIAirportResource.swift
@@ -46,15 +46,44 @@ class APIAirportResource : APIResource {
 		var error: NSError?
 		let errorinfo = ["NSLocalizedDescriptionKey":"Bad data received for from airport resource \(getAPIAddressString())"]
 
-		let data = JSON(data: webData, error: &error)["AIRPORTS"]
+		let regionData = JSON(data: webData, error: &error)["REGIONS"]
+		//		let data = JSON(data: webData, error: &error)["AIRPORTS"]
 
-		if data.error == nil {
-			var newAirports : [String] = []
+		if regionData.error == nil {
+			var newRegions : [RegionEntry] = []
+			var newDistricts : [DistrictEntry] = []
+			var newHubs : [HubEntry] = []
+			var newAirports : [AirportEntry] = []
 
-			for (index,entry) in data {
-				if let eventText = entry["FAA_CODE"].string {
-					newAirports.append(eventText)
+			for (index,region) in regionData {
+				if let regionName = region["REGION_NAME"].string {
+					let districtData = region["DISTRICTS"]
+					if districtData.type == .Array {
+						for (index, district) in districtData {
+							if let districtName = district["DISTRICT_NAME"].string {
+								let hubData = district["HUBS"]
+								if hubData.type == .Array {
+									for (index, hub) in hubData {
+										if let hubName = hub["HUB_NAME"].string {
+											let airportData = hub["AIRPORTS"]
+											if airportData.type == .Array {
+												for (index, airport) in airportData {
+													if let airportName = airport["FAA_CODE"].string {
+														// save object
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					else {
+						// error no districts in region
+					}
 				}
+
 				else {
 					error = NSError (domain: "API Airport", code: 10, userInfo: errorinfo)
 					return error
@@ -68,11 +97,39 @@ class APIAirportResource : APIResource {
 			for airport in newAirports {
 				let entityDescription = NSEntityDescription.entityForName("AirportEntry", inManagedObjectContext: managedObjectContext!)
 				let newAirportEntry = AirportEntry(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext)
-				newAirportEntry.faa_code = airport
+				//newAirportEntry.faa_code = airport
 
 				managedObjectContext?.save(&error)
 			}
 		}
+
+/*
+if data.error == nil {
+var newAirports : [String] = []
+
+for (index,entry) in data {
+if let eventText = entry["FAA_CODE"].string {
+newAirports.append(eventText)
+}
+else {
+error = NSError (domain: "API Airport", code: 10, userInfo: errorinfo)
+return error
+}
+}
+
+if newAirports.count != 0 {
+deleteOld()
+}
+
+for airport in newAirports {
+let entityDescription = NSEntityDescription.entityForName("AirportEntry", inManagedObjectContext: managedObjectContext!)
+let newAirportEntry = AirportEntry(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext)
+newAirportEntry.faa_code = airport
+
+managedObjectContext?.save(&error)
+}
+}
+*/
 		else {
 			error = NSError (domain: "API Airport", code: 11, userInfo: errorinfo)
 		}
